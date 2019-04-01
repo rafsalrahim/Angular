2283da8686471407d2af58116107a080c5bc147a
rafsal <rafsalkrahim@gmail.com>
2019-04-01 10:42:19 +0530

    component roots added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { DetailsComponent } from './details/details.component';
+import { PostsComponent } from './posts/posts.component';
 
 const routes: Routes = [];
 
